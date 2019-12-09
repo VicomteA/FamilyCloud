bbb0101e46237759b1366fc261b32962d3ca9e82
ruski <douska40@yahoo.com>
2019-12-09 18:18:32 +0100

    pageprofile

--- laravel/resources/views/profile.blade.php
+++ laravel/resources/views/profile.blade.php
@@ -4,7 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-           Tamere
+        <img src='/uploads/avatars/{{ $user->avatar }}' style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;"> 
+           <h2>Profil de {{ $user->name }}</h2>
         </div>
     </div>
 </div>
