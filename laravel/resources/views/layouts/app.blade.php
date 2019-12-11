91313c413617ae92472328cb9a2327da364d10c1
Quentin <quentin.senelas@gmail.com>
2019-12-11 16:17:56 +0100

    Maj traduction
    Retour de "Transfert" à "Upload"

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -57,7 +57,7 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
-                                    <a class="dropdown-item" href="{{ route('upload') }}">{{ __('Transfert') }}</a>
+                                    <a class="dropdown-item" href="{{ route('upload') }}">{{ __('Upload') }}</a>
 
 
                                     <a class="dropdown-item" href="{{ route('profile') }}">
